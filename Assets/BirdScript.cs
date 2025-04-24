c16ee1617fe769498af48be63a65fa55e2812991
Chris Carl <chrisbcarl@outlook.com>
2025-04-24 16:36:48 -0700

    reacts to spacebar input

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float verticalStrength = 12;
+    //public float _devOobY = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //this._gravityScale = this.rigidbody2D.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.linearVelocity = Vector2.up * this.verticalStrength;
+        }
+        //Debug.Log($"this.transform.position.y: {this.transform.position.y}, {-this._devOobY}");
+        //if (this.transform.position.y < -this._devOobY)
+        //{
+        //    this.transform.position.Set(this.transform.position.x, this._devOobY, this.transform.position.z);
+        //}
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,13 @@
 2. initial "worlds most boring game"
     - added images folder
     - created Bird game object
+        - sprite renderer
+        - dragged the image onto the sprite renderer's sprite
     - zoomed out the camera and changed background color
+3. reacts to input
+    - added RigidBody2D
+        - gravity scale to 4 (saved by SampleScene.unity)
+    - added CircleCollider2D
+    - added BirdScript.cs
+        - gravity scale to 12  (saved by BirdScript.cs)
+            - values found by experimentation in play mode, though the play settings arent sticky and will always revert to whatever the script had and whatever the scene had
